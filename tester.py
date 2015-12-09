cb72d4458e0b241d81351f61f84fcd8752e3fc6d
Marcos Kintschner <marcos.ktn@gmail.com>
2015-12-09 00:51:15 -0200

    Criada a thread para fazer o envio e controle das mensagens (parcialmente implementado).

--- tester.py
+++ tester.py
@@ -1,54 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
-
-# pacote deve ter esse formato de acordo com a rfc2544
-"""
-UDP echo request on Ethernet
-
-       -- DATAGRAM HEADER
-       offset data (hex)            description
-       00     xx xx xx xx xx xx     set to dest MAC address
-       06     xx xx xx xx xx xx     set to source MAC address
-       12     08 00                 type
-
-       -- IP HEADER
-       14     45                    IP version - 4 header length 5 4
-      byte units
-       15     00                    TOS
-       16     00 2E                 total length*
-       18     00 00                 ID
-       20     00 00                 flags (3 bits) - 0 fragment
-      offset-0
-       22     0A                    TTL
-       23     11                    protocol - 17 (UDP)
-       24     C4 8D                 header checksum*
-       26     xx xx xx xx           set to source IP address**
-       30     xx xx xx xx           set to destination IP address**
-
-       -- UDP HEADER
-       34     C0 20                 source port
-       36     00 07                 destination port 07 = Echo
-       38     00 1A                 UDP message length*
-       40     00 00                 UDP checksum
-
-       -- UDP DATA
-       42     00 01 02 03 04 05 06 07    some data***
-       50     08 09 0A 0B 0C 0D 0E 0F
-
-      * - change for different length frames
-
-      ** - change for different logical streams
-
-      *** - fill remainder of frame with incrementing octets,
-      repeated if required by frame length
-
-Values to be used in Total Length and UDP message length fields
-"""
-
 import socket
 import struct
-import sys
+import time
+import multiprocessing
 
 
 def print_binary(header):
@@ -84,22 +40,16 @@ def checksum(msg):
 
 
 def build_packet(src_ip, dst_ip):
+
+    # pacote deve ter esse formato de acordo com a rfc2544
     """
-     0                   1                   2                   3
-     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    |Version|  IHL  |Type of Service|          Total Length         |
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    |         Identification        |Flags|      Fragment Offset    |
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    |  Time to Live |    Protocol   |         Header Checksum       |
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    |                       Source Address                          |
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    |                    Destination Address                        |
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    |                    Options                    |    Padding    |
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    UDP echo request on Ethernet
+
+           -- DATAGRAM HEADER
+           offset data (hex)            description
+           00     xx xx xx xx xx xx     set to dest MAC address
+           06     xx xx xx xx xx xx     set to source MAC address
+           12     08 00                 type
 
            -- IP HEADER
            14     45                    IP version - 4 header length 5 4
@@ -114,6 +64,25 @@ def build_packet(src_ip, dst_ip):
            24     C4 8D                 header checksum*
            26     xx xx xx xx           set to source IP address**
            30     xx xx xx xx           set to destination IP address**
+
+           -- UDP HEADER
+           34     C0 20                 source port
+           36     00 07                 destination port 07 = Echo
+           38     00 1A                 UDP message length*
+           40     00 00                 UDP checksum
+
+           -- UDP DATA
+           42     00 01 02 03 04 05 06 07    some data***
+           50     08 09 0A 0B 0C 0D 0E 0F
+
+          * - change for different length frames
+
+          ** - change for different logical streams
+
+          *** - fill remainder of frame with incrementing octets,
+          repeated if required by frame length
+
+    Values to be used in Total Length and UDP message length fields
     """
 
     # ip header
@@ -136,25 +105,10 @@ def build_packet(src_ip, dst_ip):
     H = unsigned word -> [0,65535]
     4s = char[4] -> [0, 255]x4
     """
-    ip_header = struct.pack('!BBHHHBBH4s4s', ip_version, ip_tos, ip_total_length,
-                            ip_id, ip_frag_offset, ip_ttl, ip_protocol,
-                            ip_checksum, ip_src_addr, ip_dest_addr)
-
-    """
-     0                   1                   2                   3
-     0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    |          Source Port          |       Destination Port        |
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    |             Length            |           Checksum            |
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-
-          -- UDP HEADER
-           34     C0 20                 source port
-           36     00 07                 destination port 07 = Echo
-           38     00 1A                 UDP message length*
-           40     00 00                 UDP checksum
-    """
+    ip_header = struct.pack('!BBHHHBBH4s4s', ip_version, ip_tos,
+                            ip_total_length, ip_id, ip_frag_offset, ip_ttl,
+                            ip_protocol, ip_checksum, ip_src_addr,
+                            ip_dest_addr)
 
     # udp header
     udp_src_port = 0xc020   # 2 bytes
@@ -164,8 +118,8 @@ def build_packet(src_ip, dst_ip):
 
     # ! network order (big-endian)
     # H = unsigned word -> [0,65535]
-    udp_header = struct.pack("!HHHH", udp_src_port, udp_dest_port, udp_msg_length,
-                             udp_checksum)
+    udp_header = struct.pack("!HHHH", udp_src_port, udp_dest_port,
+                             udp_msg_length, udp_checksum)
 
     udp_data = "hello".encode('ascii')
 
@@ -226,21 +180,101 @@ def get_socket():
 
 PACKET_SIZES = [64, 128, 256, 512, 1024, 1280, 1518]
 
+
+def socket_send(src_ip, dst_ip, num_pkt_sent, num_pkt_recv):
+    MAX_TIME = 2
+
+    sock = get_socket()
+    packet = build_packet(src_ip, dst_ip)
+    packet_rate = 1 # packets per second
+    highest_lossless_rate = -1
+    highest_loss_rate = -1
+
+    while True:
+        period = 1/packet_rate
+        time_counter = 0.0
+        with num_pkt_sent.get_lock():
+            num_pkt_sent.value = 0
+        while time_counter < MAX_TIME:
+            print("Sending packet1 to: %s" % dst_ip)
+            sock.sendto(packet, (dst_ip, 0))
+            with num_pkt_sent.get_lock():
+                num_pkt_sent.value += 1
+            time.sleep(period)
+            time_counter += period
+        print("Terminating send process")
+        time.sleep(1) # espera os outros pacotes chegarem
+        if num_pkt_sent.value > num_pkt_recv.value:
+            if packet_rate > highest_loss_rate:
+                highest_loss_rate = packet_rate
+            if highest_lossless_rate < 0:
+                print("Throughput < 1 pps")
+                break
+            packet_rate = (highest_lossless_rate + packet_rate) / 2
+            print('num_pkt_sent ({}) > num_pkt_recv ({})'.format(
+                  num_pkt_sent.value, num_pkt_recv.value))
+            print('Reducing rate to {} pps'.format(packet_rate))
+        else:
+            if packet_rate == highest_lossless_rate:
+                print("Highest rate found: {} pps".format(packet_rate))
+                break
+            highest_lossless_rate = packet_rate
+            if 0 < highest_loss_rate <= packet*2:
+                packet_rate = (highest_loss_rate + packet_rate) / 2
+            else:
+                packet_rate *= 2
+            print('num_pkt_sent ({}) <= num_pkt_recv ({})'.format(
+                  num_pkt_sent.value, num_pkt_recv.value))
+            print('Increasing rate to {} pps'.format(packet_rate))
+
+
+def socket_recv(src_ip, dst_ip, packet_sz, num_pkt_recv):
+    sock = get_socket()
+
+    while True:
+        sz, addr = sock.recvfrom(packet_sz)
+        print('Packet received from %s' % addr)
+        num_pkt_recv.value += 1
+
 if __name__ == "__main__":
 
-    if len(sys.argv) < 2:
-        print('usage: python3 %s <DST_IP>' % sys.argv[0])
+    if len(sys.argv) < 3:
+        print('usage: python3 %s <DESTINATION_IP> <PACKET_SIZE>' % sys.argv[0])
         sys.exit()
 
     dst_ip = sys.argv[1]
     if not is_valid_ip(dst_ip):
         print('dst_ip: %s is not a valid ip address' % dst_ip)
-        sys.exit(0)
+        sys.exit()
+
+    packet_sz = int(sys.argv[2])
+    if packet_sz not in PACKET_SIZES:
+        print('{} is not a valid packet size. Packet must have on of the '
+              'follow sizes: {}.'.format(packet_sz, PACKET_SIZES))
+        sys.exit()
+
     src_ip = get_local_ip()
+
     print('src ip is %s' % src_ip)
     print('dst ip is %s' % dst_ip)
+    print('packet size is %d' % packet_sz)
 
-    sock = get_socket()
-    packet = build_packet(src_ip, dst_ip)
+    # Shared memory variables
+    num_pkt_sent = multiprocessing.Value('d', 0)
+    num_pkt_recv = multiprocessing.Value('d', 0)
 
-    sock.sendto(packet, (dst_ip, 0))
+    send_p = multiprocessing.Process(target=socket_send,
+                                     args=(src_ip, dst_ip, num_pkt_sent,
+                                           num_pkt_recv))
+    recv_p = multiprocessing.Process(target=socket_recv,
+                                     args=(src_ip, dst_ip, packet_sz,
+                                           num_pkt_recv))
+
+    recv_p.start()
+    send_p.start()
+    print("joining send_p")
+    send_p.join()
+    print("joining recv_p")
+    recv_p.join()
+    print("Ending program")
+    #sock.sendto(packet, (dst_ip, 0))
